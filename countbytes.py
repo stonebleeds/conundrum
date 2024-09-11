6fd9a0163904a57cde2db6bbfe5f1cfaba8e5547
stonebleeds <orangejacketguy@icloud.com>
2024-09-11 01:37:36 -0600

    new version

--- countbytes.py
+++ countbytes.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 
 import sys,time
 
@@ -6,16 +6,20 @@ print "Countbytes started at",time.ctime()
 
 thresh=1
 bytedict={}
+finalcount=0
 for i in range(0,256): bytedict[chr(i)]=0
 
 time.sleep(3)
 
-print "Intake loop starting at",time.ctime()
+print "detecting input... ",time.ctime()
 
 nextbyte=sys.stdin.read(1)
 while nextbyte=='':
+	print "still no input..."
 	time.sleep(1)
 	nextbyte=sys.stdin.read(1)
+
+print "Intake loop starting at",time.ctime()
 
 while nextbyte:
 	bytedict[nextbyte]+=1
@@ -27,6 +31,25 @@ while nextbyte:
 print "Loop done at",time.ctime()
 
 for i in range(0,256):
-	print hex(i)[2:].zfill(2).upper(),bytedict[chr(i)]
+	print '0x'+hex(i)[2:].zfill(2).upper()+'\t'+str(bytedict[chr(i)])
+
+for i in range(0,256):
+	finalcount+=bytedict[chr(i)]
+print finalcount,"bytes counted"
+
+highest=('',0)
+for i in range(0,256):
+	cchr,ccnt=highest
+	if ccnt<bytedict[chr(i)]: highest=chr(i),bytedict[chr(i)]
+
+cchr,ccnt=highest
+print "most occurred is probably",hex(ord(cchr)),"with",ccnt,"times"
+
+lowest='',ccnt
+for i in range(0,256):
+	cchr,ccnt=lowest
+	if ccnt>bytedict[chr(i)]: lowest=chr(i),bytedict[chr(i)]
+
+print "least occurred is probably",hex(ord(cchr)),"with",ccnt,"times"
 
 print "Countbytes done at",time.ctime()
--- main.py
+++ main.py
@@ -1,59 +1,197 @@
-#!/usr/bin/python
-# main.py (wheel positions in hex) wheels... bigwheelpos
-# ./main.py 0x000b2e1affff 39 164 41 157 60 226 0x0123
+#!/usr/bin/python2.7
+
+# setting 0x0123 0x4567
+# wheel 0x01, position 0x23 with position 0x4567 on bigwheel
+#  -or-
+# setting 0x89AB 0xCDEF 0x3030
+# wheels 0x89 and 0xCD, with positions 0xAB and 0xEF respectively,
+# with position 0x3030 on bigwheel
+
+
+# because I don't want to write the code to handle for otherwise right
+# now, each instance of conundrum (for the time being) must have
+# one wheel w/ position AND a position on the bigwheel
+# (I do want to EVENTUALLY write code that will handle just ONE 16-bit
+# number, and that will mean a position on the bigwheel)
+
 
 import precon, sys, time, os
 
-version="0.01nu"
+version="0.03ab"
+sleepintervalmax = 5
 
 flushline=0
+outcount=0
+lastflush=time.time()
+flushlinelimit=40
 
 diagnosticlines=[]
 
-diagnosticlines.append("Started precon "+version+" with options:\r\n")
+def diagwriteout():
+    global diagnosticlines, outcount
+    #flname=str(int(time.time()))+"diag.txt"
+    flname="diag"+str(int(time.time()))
+    for i in sys.argv[1:]:flname=flname+"_"+i
+    while "0x" in flname: flname=flname.replace("0x",'')
+    if len(flname)>240: flname=flname[:240]+"V"
+    flname=flname+".txt"
+    while os.path.exists(flname):
+        diagnosticlines.append(time.ctime()+" No joke, "+flname+" seems to exist. Waiting...\r\n")
+        time.sleep(0.9)
+        flname="diag"+str(int(time.time()))
+        for i in sys.argv[1:]:flname=flname+"_"+i
+        if len(flname)>240: flname=flname[:240]+"V"
+        flname=flname+".txt"
+    diagnosticlines.append("Final count appears to be "+str(outcount)+" / "+hex(outcount)+"\r\n")
+    diagnosticlines.append(time.ctime()+" Writing diagnostics to "+flname+"\r\n")    
+    open(flname,'wb').writelines(diagnosticlines)
+
+work=sys.argv[1:]
+bigwheelpos=int(work.pop(-1)[2:],16)
+
+diagnosticlines.append("Started conundrum "+version+" with precon.py version "+precon.PRECONVER+" and options:\r\n")
 for i in sys.argv: diagnosticlines.append(i+' ')
-
 diagnosticlines.append("\r\nat "+time.ctime()+"\r\n")
+diagnosticlines.append("Work list shows up as "+str(work)+" <---\r\n")
+diagnosticlines.append("bigwheelpos appears to be "+hex(bigwheelpos)+"\r\n")
 
-w1=precon.WHEELS[int(sys.argv[2])]# 40 bits shifted
-w2=precon.WHEELS[int(sys.argv[3])]# 32 bits shifted
-w3=precon.WHEELS[int(sys.argv[4])]# 24 bits shifted
-w4=precon.WHEELS[int(sys.argv[5])]# 16 bits shifted
-w5=precon.WHEELS[int(sys.argv[6])]# 8 bits shifted
-w6=precon.WHEELS[int(sys.argv[7])]# 0 bits shifted
+cassette=[]
+#wheelpos=0x0
+wheelposstr=""
+cassettelength=len(work)
 
-bigwheelpos=int(sys.argv[8][2:],16)
+diagnosticlines.append("cassette length is figured to be "+str(cassettelength)+" / "+hex(cassettelength)+"\r\n")
 
-diagnosticlines.append(time.ctime()+" Defined stuff, doing whlpos loop\r\n")
+while work:
+    workl=work.pop(0)
+    diagnosticlines.append("workl appears to be "+workl+"\r\n")
+    setting=workl[2:].zfill(4)
+    wheelstr=setting[:2]
+    posstr=setting[2:]
+    diagnosticlines.append("adding setting "+setting+" with wheel "+wheelstr+" at position "+posstr+"\r\n")
+    cassette.append(precon.WHEELS[int(wheelstr,16)])
+    wheelposstr=wheelposstr+posstr
+    diagnosticlines.append("wheelposstr appears to be "+wheelposstr+"\r\n")
 
-whlpos=int(sys.argv[1][2:],16)
+wheelpos=int(wheelposstr,16)
 
-#time.sleep(3)
+diagnosticlines.append("preliminary wheelpos is "+hex(wheelpos)+"\r\n")
+
+'''
+while work:
+    wheelpos=wheelpos<<8
+    workl = work.pop(0)
+    diagnosticlines.append("workl appears to be "+workl+"\r\n")
+    setting=workl[2:].zfill(4)
+    wheel=int(setting[:2],16)
+    position=int(setting[2:],16)
+    diagnosticlines.append("adding setting "+setting+" with wheel "+hex(wheel)+" at position "+hex(position)+"\r\n")
+    cassette.append(precon.WHEELS[wheel])
+    wheelpos+=position
+    diagnosticlines.append("wheelpos is now at "+hex(wheelpos)+"\r\n")
+    if wheelpos==0:
+        diagnosticlines.append("First wheel position cannot be zero in this version. I'd apologize, but whatever.\r\n")
+        diagwriteout()
+        raise RuntimeError, "unsupported wheel position"
+'''
+
+diagnosticlines.append("Moving on...\r\n")
+wheelposmask=(256**cassettelength)-1
+diagnosticlines.append("wheelposmask is figured to be "+hex(wheelposmask)+"\r\n")
+
+wheelpos=wheelpos&wheelposmask
+
+diagnosticlines.append("wheelpos is figured to be "+hex(wheelpos)+"\r\n")
+
+diagnosticlines.append("starting intake loops at "+time.ctime()+"\r\n")
+
+# these two lines (and the following loop, for that matter) are there because
+# some processes don't start sending data through the pipe in a timely fashion
+# this is sometimes referred to as the "delay timer"
+sleepinterval=0
+time.sleep(sleepinterval)
 
 nextbyte=sys.stdin.read(1)
-while nextbyte=='':
-	time.sleep(3)
-	nextbyte=sys.stdin.read(1)
-	diagnosticlines.append(time.ctime()+" Triggering empty next byte...\r\n")
+while nextbyte=="": # this is the "delay timer"
+    sleepinterval+=1
+    if sleepinterval >= sleepintervalmax:
+        diagnosticlines.append(time.ctime()+" Sleep interval exceeded set maximum of "+str(sleepintervalmax)+" seconds before processing loops. Calling it.")
+        diagwriteout()
+        raise RuntimeError, "Never got incoming bytes and hit interval limit."
+    diagnosticlines.append(time.ctime()+" nextbyte empty before processing loops. Sleep interval currently at "+str(sleepinterval)+".\r\n")
+    time.sleep(sleepinterval)
+    diagnosticlines.append(time.ctime()+" Trying again...\r\n")
+    nextbyte=sys.stdin.read(1)
 
 while nextbyte!='':
-	if whlpos==0: diagnosticlines.append(time.ctime()+" Wheel positions are at 0\r\n")
-	xr=w1[(whlpos>>40)&0xff]^w2[(whlpos>>32)&0xff]^w3[(whlpos>>24)&0xff]^w4[(whlpos>>16)&0xff]^w5[(whlpos>>16)&0xff]^w6[whlpos&0xff]^precon.BW[bigwheelpos]
-	sys.stdout.write(chr(ord(nextbyte)^xr))
-	flushline+=1
-	if flushline>40:
-                sys.stdout.flush()
-                flushline=0
-	whlpos+=1
-	whlpos=whlpos&0xffffffffffff
-	bigwheelpos+=1
-	bigwheelpos=bigwheelpos&0xffff
-	nextbyte=sys.stdin.read(1)
+    if wheelpos==0: diagnosticlines.append(time.ctime()+" Wheel positions are at 0\r\n")
+    xr = 0
+    positionbytes=hex(wheelpos)[2:].zfill(2*cassettelength)
+    if positionbytes[-1]=='L':
+        positionbytes=positionbytes[:-1].zfill(2*cassettelength)
+    positionbytelist=[positionbytes[j:j+2].zfill(2) for j in range(0, len(positionbytes), 2)]
+    if positionbytelist[-1]=='L':
+        waste=positionbytelist.pop(-1)
+    for x in range(0,cassettelength):
+        try: xr = xr ^ cassette[x][int(positionbytelist[x],16)]
+        except ValueError:
+            diagnosticlines.append("Choked at xr calculation with a ValueError at "+time.ctime()+'\r\n')
+            diagnosticlines.append("positionbytelist looks like:\r\n")
+            diagnosticlines.append(str(positionbytelist)+"\r\n")
+            diagwriteout()
+            raise RuntimeError, "choked and wrote out"
+    xr = xr ^ precon.BW[bigwheelpos]
+    sys.stdout.write(chr(ord(nextbyte)^xr))
+    outcount+=1
+    flushline+=1
+    if flushline>flushlinelimit:
+        try:
+            sys.stdout.flush()
+            # trying to have this just be code that has the script flushing stdout at a consistent pace -- I am probably failing at this
+            if time.time()-lastflush < 3: flushlinelimit+=1
+            if time.time()-lastflush > 4: flushlinelimit-=1
+            lastflush=time.time()
+        except IOError:
+            diagnosticlines.append("Choked on an IOError while trying to flush the stdout with positionbytelist looking like:\r\n")
+            diagnosticlines.append(str(positionbytelist)+"\r\n")
+            diagnosticlines.append("bigwheelpos looks like:"+hex(bigwheelpos)+"\r\n")
+            diagnosticlines.append("Might be a case of unrecommended stacking...\r\n")
+            diagwriteout()
+            raise RuntimeError, "choked and wrote out"
+        flushline=0
+        #spot check
+    if hex(outcount)[-5:]=='18680':
+        diagnosticlines.append("Spot check triggered at "+time.ctime()+"\r\n")
+        diagnosticlines.append("positionbytelist looks like:\r\n")
+        diagnosticlines.append(str(positionbytelist)+"\r\n")
+        diagnosticlines.append("bigwheelpos looks like:"+hex(bigwheelpos)+"\r\n")
+    wheelpos+=1
+    wheelpos=wheelpos&wheelposmask
+    bigwheelpos+=1
+    bigwheelpos=bigwheelpos&0xffff
+    #if not bigwheelpos: diagnosticlines.append("bigwheel position appears to be zero at "+time.ctime()+"\r\n")
+    nextbyte=sys.stdin.read(1)
 
-diagnosticlines.append(time.ctime()+" Wheel positions are 0x"+hex(whlpos)[2:].upper().zfill(12)+'\r\n')
+diagnosticlines.append(time.ctime()+" Intake loops appear to be done.\r\n")
+diagnosticlines.append(time.ctime()+" Wheel positions are "+hex(wheelpos)+"\r\n")
+
+#might remove this part later -- reporting final positions and xrs
+positionbytes=hex(wheelpos)[2:].zfill(2*cassettelength)
+if positionbytes[-1]=='L':
+    positionbytes=positionbytes[:-1].zfill(2*cassettelength)
+diagnosticlines.append("positionbytes appears to be "+positionbytes+"\r\n")
+positionbytelist=[positionbytes[j:j+2].zfill(2) for j in range(0, len(positionbytes), 2)]
+#positionbytelist=[positionbytes[j:j+2] for j in range(0, len(positionbytes), 2)]
+diagnosticlines.append("positionbytelist looks like "+str(positionbytelist)+"\r\n")
+lxr=0
+diagnosticlines.append("xrs appear to be:\r\n")
+for x in range(0,cassettelength):
+    lxr = lxr ^ cassette[x][int(positionbytelist[x],16)]
+    diagnosticlines.append(hex(lxr)+" ")
+diagnosticlines.append(" <----\r\n")
+diagnosticlines.append("bigwheel at position "+hex(bigwheelpos)+" is "+hex(precon.BW[bigwheelpos])+"\r\n")
+diagnosticlines.append("last xr was "+hex(xr)+"\r\n")
+
 diagnosticlines.append(time.ctime()+" Done.\r\n")
-flname=str(int(time.time()))+"diag.txt"
-while os.path.exists(flname):
-	time.sleep(0.9)
-	flname=str(int(time.time()))+"diag.txt"
-open(flname,'wb').writelines(diagnosticlines)
+
+diagwriteout()
